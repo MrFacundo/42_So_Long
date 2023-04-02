70571fd22c86bc4aa9bf95d6581986fc473537c7
Facundo Troitero <ftroiter@c2r5s2.42lisboa.com>
2023-04-02 22:01:53 +0100

    rcommit

--- srcs/main.c
+++ srcs/main.c
@@ -5,88 +5,79 @@
 
 #include "../includes/window.h"
 
-// void *win_ptr;
 
-void ft_putchar_fd(char c, int fd)
+int     exit_program(t_program program)
 {
-	write(fd, &c, 1);
-}
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t	i;
-
-	if (!dst && !src)
-		return (NULL);
-	i = 0;
-	while (i < n)
-	{
-		*(char *)(dst + i) = *(char *)(src + i);
-		i++;
-	}
-	return (dst);
-}
-
-/* 
-int handleKey(int keycode, void *param)
-{
-	ft_putchar_fd(keycode, 1);
-	if (keycode == 'x')
-		mlx_destroy_window(my_window.mlx_ptr, my_window.win_ptr);
-	return (0);
-} */
-
-t_window	new_window(int width, int height, char	*name)
-{
-	void		*mlx_ptr;
-	t_window	new_window;	
-	
-	mlx_ptr = mlx_init();
-	new_window.mlx_ptr = mlx_ptr;
-	new_window.win_ptr = mlx_new_window(mlx_ptr, width, height, name);
-	new_window.width = width;
-	new_window.height = height;
-
-	return (new_window);
-}
-
-t_img	new_img(t_window my_window, int height, int width)
-{
-	t_img	new_image;
-	
-	new_image.win = my_window;
-	new_image.img_ptr = mlx_new_image(my_window.mlx_ptr, width, height);
-	new_image.addr = mlx_get_data_addr(new_image.img_ptr, &(new_image.bpp), &(new_image.line_len), &(new_image.endian));
-	new_image.height = 4;
-	new_image.width = 4;
-	
-	printf("img_ptr: %p\n",new_image.img_ptr);
-	printf("bpp: %d\n",new_image.bpp);
-	printf("endian: %d\n",new_image.endian);
-	printf("line_len: %d\n",new_image.line_len);
-	printf("addr: %p\n",new_image.addr);
-
-	return (new_image);
-}
-
-int     exit_tutorial(t_window *window)
-{
-	if (window)
-		mlx_destroy_window (window->mlx_ptr, window->win_ptr);
+	printf("closing");
+	mlx_destroy_window (program.mlx_ptr, program.window.win_ptr);
 	exit(0);
+}
+
+t_window	new_window(t_program program , int width, int height, char	*name)
+{
+	t_window	window;	
+
+	window.win_ptr = mlx_new_window(program.mlx_ptr, width, height, name);
+	window.size.x = width;
+	window.size.y = height;
+	mlx_hook(window.win_ptr, 17, 0, exit_program, &program);
+	return (window);
+}
+
+t_image new_img(void *mlx_ptr, char *path)
+{
+	t_image image;
+	
+	printf("mlx_ptr: %p\n",mlx_ptr);
+	printf("path: %s\n",path);
+	printf("image.size.x: %d\n",image.size.x);
+	image.img_ptr = mlx_xpm_file_to_image(mlx_ptr, path, &image.size.x, &image.size.y);
+	printf("img_ptr: %p\n",image.img_ptr);
+	
+	image.pixels  = mlx_get_data_addr(image.img_ptr, &image.bits_per_pixel, &image.line_len, &image.endian);
+	
+	printf("bpp: %d\n",image.bits_per_pixel);
+	printf("endian: %d\n",image.endian);
+	printf("line_len: %d\n",image.line_len);
+	printf("addr: %p\n",image.pixels);
+	
+	return (image);
+
+}
+
+int	handle_key(int key, void *param)
+{
+	t_program *program = (t_program *)param;
+
+	mlx_clear_window(program->mlx_ptr, program->window.win_ptr);
+	if (key == 'd')
+		program->sprite_position.x += program->sprite.size.x;
+	else if (key == 'a')
+		program->sprite_position.x -= program->sprite.size.x;
+	else if (key == 'w')
+		program->sprite_position.y += program->sprite.size.y;
+	else if (key == 's')
+		program->sprite_position.y -= program->sprite.size.y;
+	else if (key == 'x')
+		mlx_destroy_window(program->mlx_ptr, program->window.win_ptr);
+	mlx_put_image_to_window(program->mlx_ptr, program->window.win_ptr,
+		program->sprite.img_ptr, program->sprite_position.x, program->sprite_position.y);
+
+	printf("Key pressed -> %d\n", key);
+	return (0);
 }
 
 int main()
 {
-	t_window	my_window;	
-	t_img	my_image;
-
-	my_window = new_window(500, 500, "myWindow");
-	// mlx_key_hook(my_window.win_ptr, handleKey, (void *)0);
-	my_image = new_img(my_window, 4, 4);
-	ft_memcpy(my_image.addr, "s4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vfs4vf", 16*4);
-	mlx_put_image_to_window(my_image.win.mlx_ptr, my_image.win.win_ptr, my_image.img_ptr, 10, 10);
-	mlx_hook(my_window.win_ptr, 17, 0, exit_tutorial, &my_window);
-	mlx_loop(my_window.mlx_ptr);
+	t_program program;
+	
+	program.mlx_ptr = mlx_init();
+	program.window = new_window(program, 500, 500, "myWindow");
+	program.sprite = new_img(program.mlx_ptr, "block.xpm");
+	program.sprite_position.x = 0;
+	program.sprite_position.y = 0;
+	mlx_put_image_to_window(program.mlx_ptr, program.window.win_ptr, program.sprite.img_ptr, program.sprite_position.x, program.sprite_position.y);
+	// mlx_key_hook(program.window.win_ptr, *handle_key, &program);
+	mlx_loop(program.mlx_ptr);
 	return(0);
 }
